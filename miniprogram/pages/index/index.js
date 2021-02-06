e20e584dbc326a7391f2d66d3c9de20b75c80373
LiangJavaDS <954157304@qq.com>
2021-02-06 16:12:41 +0800

    第一个静态页面
    第一个静态页面

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,6 +10,14 @@ Page({
     requestResult: ''
   },
 
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+
   onLoad: function() {
     if (!wx.cloud) {
       wx.redirectTo({
